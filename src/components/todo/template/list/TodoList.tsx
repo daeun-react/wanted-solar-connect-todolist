a6139f2f2d2ddb8f611df7f488d22f32454151ab
daeun-react <smile_daeun@naver.com>
2021-08-20 23:01:34 +0900

    feat : todo-list 없는 경우를 보여주는 마크업 코드 추가

--- src/components/todo/template/list/TodoList.tsx
+++ src/components/todo/template/list/TodoList.tsx
@@ -1,6 +1,8 @@
-import { Itodo } from "components/todo/TodoService";
 import React from "react";
 import styled from "styled-components";
+import { Result } from "antd";
+import { SmileTwoTone } from "@ant-design/icons";
+import { Itodo } from "components/todo/TodoService";
 import TodoItem from "./item/TodoItem";
 
 const TodoListBlock = styled.div`
@@ -10,6 +12,13 @@ const TodoListBlock = styled.div`
   overflow-y: auto;
 `;
 
+const BlockCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
 interface TodoListProps {
   todos: Itodo[];
   toggleTodo: (id: number) => void;
@@ -17,6 +26,18 @@ interface TodoListProps {
 }
 
 const TodoList = ({ toggleTodo, removeTodo, todos }: TodoListProps) => {
+  if (todos.length === 0)
+    return (
+      <BlockCenter>
+        <Result
+          icon={
+            <SmileTwoTone twoToneColor="#ff6b6b" style={{ fontSize: "60px" }} />
+          }
+          subTitle="Please add to-do list."
+        />
+      </BlockCenter>
+    );
+
   return (
     <TodoListBlock>
       {todos &&
