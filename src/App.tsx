57111c5f3bb790bf837382d58f15e957246c6d06
daeun-react <smile_daeun@naver.com>
2021-08-20 23:10:10 +0900

    feat : 로그인 시 1초 Spinner 보여주고 화면 전환 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import "antd/dist/antd.css";
 import { load, save, remove } from "utils/localStorage";
 import LoginModal from "components/login/LoginModal";
 import TodoContainer from "./components/todo/TodoContainer";
+import Spinner from "./components/common/Spinner";
 
 function App() {
   const [isLogged, setIsLogged] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [loginModalVisible, setLoginModalVisible] = useState(false);
 
   const onLogin = (username: string) => {
-    save("todo-user", username);
-    setIsLogged(true);
+    setLoginModalVisible(false);
+    setLoading(true);
+    setTimeout(() => {
+      save("todo-user", username);
+      setIsLogged(true);
+      setLoading(false);
+    }, 1000);
   };
 
   const onLogout = () => {
@@ -32,7 +39,10 @@ function App() {
   return isLogged ? (
     RenderLayout
   ) : (
-    <LoginModal visible={loginModalVisible} onLogin={onLogin} />
+    <>
+      <LoginModal visible={loginModalVisible} onLogin={onLogin} />
+      {loading && <Spinner mask />}
+    </>
   );
 }
 
