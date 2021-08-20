292cd573ca69294465e7c716419f66c80b0b0595
daeun-react <smile_daeun@naver.com>
2021-08-21 00:57:21 +0900

    feat : todo-list 입력순/마감임박순 정렬 기능 구현

--- src/components/todo/template/list/TodoList.tsx
+++ src/components/todo/template/list/TodoList.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { Result } from "antd";
-import { SmileTwoTone } from "@ant-design/icons";
+import { Result, Button } from "antd";
+import { SmileTwoTone, CaretDownOutlined } from "@ant-design/icons";
 import { Itodo } from "components/todo/TodoService";
 import TodoItem from "components/todo/template/list/item/TodoItem";
 
@@ -11,7 +11,29 @@ interface TodoListProps {
   removeTodo: (id: number) => void;
 }
 
+type ButtonTypeProps =
+  | "default"
+  | "link"
+  | "text"
+  | "ghost"
+  | "primary"
+  | "dashed"
+  | undefined;
+
 const TodoList = ({ toggleTodo, removeTodo, todos }: TodoListProps) => {
+  const [sortType, setSortType] = useState("id");
+
+  const ButtonType = (type: string): ButtonTypeProps =>
+    sortType === type ? "primary" : "default";
+
+  const onButtonClick = (type: string) => setSortType(type);
+
+  const onSort = (prev: Itodo, next: Itodo) =>
+    sortType === "id"
+      ? prev.id - next.id
+      : Date.parse(prev.deadline.toString()) -
+        Date.parse(next.deadline.toString());
+
   if (todos.length === 0)
     return (
       <BlockCenter>
@@ -26,15 +48,39 @@ const TodoList = ({ toggleTodo, removeTodo, todos }: TodoListProps) => {
 
   return (
     <TodoListBlock>
+      <ButtonGroup>
+        <Button
+          size="small"
+          style={{ width: "120px" }}
+          danger
+          type={ButtonType("id")}
+          onClick={() => onButtonClick("id")}
+          icon={<CaretDownOutlined />}
+        >
+          입력순
+        </Button>
+        <Button
+          size="small"
+          style={{ width: "120px" }}
+          danger
+          type={ButtonType("date")}
+          onClick={() => onButtonClick("date")}
+          icon={<CaretDownOutlined />}
+        >
+          마감임박순
+        </Button>
+      </ButtonGroup>
       {todos &&
-        todos.map((todo) => (
-          <TodoItem
-            toggleTodo={toggleTodo}
-            removeTodo={removeTodo}
-            key={todo.id}
-            todo={todo}
-          />
-        ))}
+        todos
+          .sort(onSort)
+          .map((todo) => (
+            <TodoItem
+              toggleTodo={toggleTodo}
+              removeTodo={removeTodo}
+              key={todo.id}
+              todo={todo}
+            />
+          ))}
     </TodoListBlock>
   );
 };
@@ -51,6 +97,16 @@ const TodoListBlock = styled.div`
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 10px 0;
+
+  Button + Button {
+    margin-left: 4px;
+  }
 `;
 
 export default React.memo(TodoList);
